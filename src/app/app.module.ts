4f781fa3a2fb572f79f3451395dbb5a1940ebb26
RobertoSanchez18 <roberto.sanchez@vallegrande.edu.pe>
2023-09-04 20:26:08 -0500

    portfolio v.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
